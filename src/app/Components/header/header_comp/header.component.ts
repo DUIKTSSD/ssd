cffd0877a38a48ac2c2e985616fb9edb02e52f13
Anton <fpbidni@gmail.com>
2024-04-30 19:12:55 +0200

    Almost done introduce screen

--- src/app/Components/header/header_comp/header.component.ts
+++ src/app/Components/header/header_comp/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {NavbarModuleComponent} from "../../../Modules/navbar-module/navbar-module.component";
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header_comp',
   standalone: true,
   imports: [
     NavbarModuleComponent
--- src/app/Components/main/introduce-screen/introduce-screen.component.ts
+++ src/app/Components/main/introduce-screen/introduce-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "../../header/header/header.component";
+import {HeaderComponent} from "../../header/header_comp/header.component";
 
 @Component({
   selector: 'app-introduce-screen',
