097afd3d30724f9b766e7101d16f58accd327307
Anton <fpbidni@gmail.com>
2024-05-06 17:57:14 +0200

    done video_section :D

--- src/app/Pages/about-page/about-page.component.ts
+++ src/app/Pages/about-page/about-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {IntroduceScreenComponent} from "../../Components/main/introduce-screen/introduce-screen.component";
+import {VideoScreenComponent} from "../../Components/main/video-screen/video-screen.component";
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
   imports: [
-    IntroduceScreenComponent
+    IntroduceScreenComponent,
+    VideoScreenComponent
   ],
   templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.css'
+  styleUrl: './about-page.component.scss'
 })
 export class AboutPageComponent {
 
