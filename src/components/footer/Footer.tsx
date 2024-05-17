bd722d05c98cea87b72f067799dbf43a389083e8
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-17 19:29:11 +0300

    add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,46 @@
+import styles from './footer.module.scss';
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footer__logo}>
+                <img src={"/src/assets/footer_logo.svg"} alt={"Footer Logo"}/>
+            </div>
+            <div className={styles.footer__information}>
+                <div className={styles.footer__information_socialmedia}>
+                    <h2 className={styles.footer__text}>Social Media</h2>
+                    <div className={styles.footer__information_socialmedia_item}>
+                        <img src={"/src/assets/instagram_logo.svg"} alt={"Instagram Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
+                    </div>
+                    <div className={styles.footer__information_socialmedia_item}>
+                        <img src={"/src/assets/telegram_logo.svg"} alt={"Telegram Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
+                    </div>
+                    <div className={styles.footer__information_socialmedia_item}>
+                        <img src={"/src/assets/tiktok_logo.svg"} alt={"Tiktok Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
+                    </div>
+                </div>
+                <div className={styles.footer__information_contacts}>
+                    <h2 className={styles.footer__text}>Contacts</h2>
+                    <div className={styles.footer__information_contacts_item}>
+                        <img src={"/src/assets/sms_logo.svg"} alt={"Email Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
+                    </div>
+                    <div className={styles.footer__information_contacts_item}>
+                        <img src={"/src/assets/phone_logo.svg"} alt={"Phone Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>+380-099-786-46-78</a>
+                    </div>
+                    <div className={styles.footer__information_contacts_item}>
+                        <img src={"/src/assets/phone_logo.svg"} alt={"Phone Logo"}/>
+                        <a className={styles.footer__text} href={"#"}>+380-099-786-46-78</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
