ade6676b76e0b38789ce354644e3aaa966b5d080
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-24 15:51:02 +0300

    fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,45 +1,34 @@
-import styles from './footer.module.scss';
-import React from "react";
-
+import styles from './footer.module.scss'
+import React from "react"
+import footerLogoImg from "../../assets/footer_logo.svg"
+import instagramLogoImg from "../../assets/instagram_logo.svg"
+import telegramLogoImg from "../../assets/telegram_logo.svg"
+import tiktokLogoImg from "../../assets/tiktok_logo.svg"
+import smsLogoImg from "../../assets/sms_logo.svg"
+import phoneLogoImg from "../../assets/phone_logo.svg"
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
-            {/*!!! Оберни все в контейнер, посмотри другие компоненты.*/}
-            <div className={styles.footer__logo}>
-                {/* Ссылки каритнок выноси в переменные. Посмотри другие компоненты. */}
-                
-                <img src={"/src/assets/footer_logo.svg"} alt={"Footer Logo"}/>
-            </div>
-            <div className={styles.footer__information}>
-                <div className={styles.footer__information_socialmedia}>
-                    <h2 className={styles.footer__text}>Social Media</h2>
-                    {/*!!! Делай списком, итемы пишками, без ссылок.*/}
-                    <div className={styles.footer__information_socialmedia_item}>
-                        <img src={"/src/assets/instagram_logo.svg"} alt={"Instagram Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
-                    </div>
-                    <div className={styles.footer__information_socialmedia_item}>
-                        <img src={"/src/assets/telegram_logo.svg"} alt={"Telegram Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
-                    </div>
-                    <div className={styles.footer__information_socialmedia_item}>
-                        <img src={"/src/assets/tiktok_logo.svg"} alt={"Tiktok Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
-                    </div>
+        <footer>
+            <div className={styles.footer}>
+                <div className={styles.footer__logo}>
+                    <img src={footerLogoImg} alt={"Footer Logo"}/>
                 </div>
-                <div className={styles.footer__information_contacts}>
-                    <h2 className={styles.footer__text}>Contacts</h2>
-                    <div className={styles.footer__information_contacts_item}>
-                        <img src={"/src/assets/sms_logo.svg"} alt={"Email Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>@duikt_info</a>
+                <div className={styles.footer__information}>
+                    <div className={styles.footer__information_socialmedia}>
+                        <h2 className={styles.footer__text}>Social Media</h2>
+                        <ul>
+                            <li className={styles.footer__information_item}><img src={instagramLogoImg} alt={"Instagram Logo"}/><p className={styles.footer__text}>@duikt_info</p></li>
+                            <li className={styles.footer__information_item}><img src={telegramLogoImg} alt={"Telegram Logo"}/><p className={styles.footer__text}>@duikt_info</p></li>
+                            <li className={styles.footer__information_item}><img src={tiktokLogoImg} alt={"Tiktok Logo"}/><p className={styles.footer__text}>@duikt_info</p></li>
+                        </ul>
                     </div>
-                    <div className={styles.footer__information_contacts_item}>
-                        <img src={"/src/assets/phone_logo.svg"} alt={"Phone Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>+380-099-786-46-78</a>
-                    </div>
-                    <div className={styles.footer__information_contacts_item}>
-                        <img src={"/src/assets/phone_logo.svg"} alt={"Phone Logo"}/>
-                        <a className={styles.footer__text} href={"#"}>+380-099-786-46-78</a>
+                    <div className={styles.footer__information_socialmedia}>
+                        <h2 className={styles.footer__text}>Contacts</h2>
+                        <ul>
+                            <li className={styles.footer__information_item}><img src={smsLogoImg} alt={"SMS Logo"}/><p className={styles.footer__text}>@duikt_info</p></li>
+                            <li className={styles.footer__information_item}><img src={phoneLogoImg} alt={"Phone Logo"}/><p className={styles.footer__text}>+380-099-786-46-78</p></li>
+                            <li className={styles.footer__information_item}><img src={phoneLogoImg} alt={"Phone Logo"}/><p className={styles.footer__text}>+380-099-786-46-78</p></li>
+                        </ul>
                     </div>
                 </div>
             </div>
