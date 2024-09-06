02fc5ac44370f6c95deda7e3ebe2d9e83bfddc7b
Anton <fpbidni@gmail.com>
2024-09-06 11:22:06 +0200

    made links to register & login page (temporarily)

--- src/modules/navbar_module/Navbar.tsx
+++ src/modules/navbar_module/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from "./navbar.module.scss";
 import Hamburger from "../hamburger-menu_module/Hamburger";
-
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
 
@@ -12,13 +12,15 @@ const Navbar = () => {
             <div className={styles.navbar}>
                 <div className={styles.navbar__nav}>
                     <ul className={`${styles.navbar__items} ${isActive ? styles.navbar__items_active : ''}`}>
-                    <li><a href="#">Документація</a></li>
-                    <li><a href="#">Проєкти</a></li>
-                    <li><a href="#">Галерея</a></li>
-                    <li><a href="#">Новини</a></li>
-                    <li><a href="#">Про нас</a></li>
-                    <li><a href="#">Контакти</a></li>
-                    <li><a href="#">Колектив</a></li>
+                    <li><Link to="/documentation">Документація</Link></li>
+                    <li><Link to="/projects">Проєкти</Link></li>
+                    <li><Link to="/gallery">Галерея</Link></li>
+                    <li><Link to="/news">Новини</Link></li>
+                    <li><Link to="/about">Про нас</Link></li>
+                    <li><Link to="/contacts">Контакти</Link></li>
+                    <li><Link to="/team">Колектив</Link></li>
+                    <li><Link to="/login">Логін</Link></li>
+                    <li><Link to="/signup">Реєстрація</Link></li>
                 </ul>
             </div>
             <Hamburger onClick={() => setIsActive(!isActive)} className={styles.navbar__burger}/>
