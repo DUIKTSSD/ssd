a9ec550e7121ad49101e30c42953a3d21b014b9b
Anton <fpbidni@gmail.com>
2024-10-16 17:55:22 +0200

    redesigned main page

--- client/src/components/mainPage/footer/Footer.tsx
+++ client/src/components/mainPage/footer/Footer.tsx
@@ -3,14 +3,13 @@ import instagramLogoImg from "../../../assets/instagram_logo.svg"
 import telegramLogoImg from "../../../assets/telegram_logo.svg"
 import tiktokLogoImg from "../../../assets/tiktok_logo.svg"
 import smsLogoImg from "../../../assets/sms_logo.svg"
-import phoneLogoImg from "../../../assets/phone_logo.svg"
+import footerLogo from "../../../assets/footer_logo.svg";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__container}>
                 <div className={styles.footer__information}>
                     <div className={styles.footer__information_socialmedia}>
-                        <h2 className={styles.footer__text}>Social Media</h2>
                         <ul>
                             <li className={styles.footer__information_item}><img src={instagramLogoImg}
                                                                                  alt={"Instagram Logo"}/>
@@ -18,22 +17,18 @@ const Footer = () => {
                             <li className={styles.footer__information_item}><img src={telegramLogoImg}
                                                                                  alt={"Telegram Logo"}/>
                                 <p className={styles.footer__text}>@duikt_info</p></li>
-                            <li className={styles.footer__information_item}><img src={tiktokLogoImg}
-                                                                                 alt={"Tiktok Logo"}/>
-                                <p className={styles.footer__text}>@duikt_info</p></li>
+
                         </ul>
                     </div>
-                    <div className={styles.footer__information_socialmedia}>
-                        <h2 className={styles.footer__text}>Contacts</h2>
-                        <ul>
-                            <li className={styles.footer__information_item}><img src={smsLogoImg} alt={"SMS Logo"}/><p
-                                className={styles.footer__text}>@duikt_info</p></li>
-                            <li className={styles.footer__information_item}><img src={phoneLogoImg} alt={"Phone Logo"}/>
-                                <p className={styles.footer__text}>+380-099-786-46-78</p></li>
-                            <li className={styles.footer__information_item}><img src={phoneLogoImg} alt={"Phone Logo"}/>
-                                <p className={styles.footer__text}>+380-099-786-46-78</p></li>
-                        </ul>
-                    </div>
+                    <ul>
+                        <li className={styles.footer__information_item}><img src={tiktokLogoImg}
+                                                                             alt={"Tiktok Logo"}/>
+                            <p className={styles.footer__text}>@duikt_info</p></li>
+
+                        <li className={styles.footer__information_item}><img src={smsLogoImg} alt={"SMS Logo"}/><p
+                            className={styles.footer__text}>@duikt_info</p></li>
+                    </ul>
+                    <img className={styles.footer__logo} src={footerLogo} alt="logo"/>
                 </div>
             </div>
         </footer>
--- client/src/modules/navbar_module/Navbar.tsx
+++ client/src/modules/navbar_module/Navbar.tsx
@@ -8,24 +8,21 @@ const Navbar: React.FC = () => {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <div>
-            <div className={styles.navbar}>
-                 <Hamburger onClick={() => setIsActive(!isActive)} className={styles.navbar__burger}/>
-                <img className={styles.navbar__logo} src={headerLogo} alt="logo"/>
-                <ul className={`${styles.navbar__items} ${isActive ? styles.navbar__items_active : ''}`}>
-                    <li className={styles.navbar__item}><Link to='/documentation'>Документація</Link></li>
-                    <li className={styles.navbar__item}><Link to='/projects'>Проєкти</Link></li>
-                    <li className={styles.navbar__item}><Link to='/gallery'>Галерея</Link></li>
-                    <li className={styles.navbar__item}><Link to='/news'>Новини</Link></li>
-                    <li className={styles.navbar__item}><Link to='/'>Про нас</Link></li>
-                    <li className={styles.navbar__item}><Link to='/contacts'>Контакти</Link></li>
-                    <li className={styles.navbar__item}><Link to='/team'>Колектив</Link></li>
-                </ul>
-                <button className={styles.navbar__auth_btn}>
-                    <Link to="/signup">Вхід/Реєстрація</Link>
-                </button>
+        <div className={styles.navbar}>
+             <Hamburger onClick={() => setIsActive(!isActive)} className={styles.navbar__burger}/>
+            <img className={styles.navbar__logo} src={headerLogo} alt="logo"/>
+            <ul className={`${styles.navbar__items} ${isActive ? styles.navbar__items_active : ''}`}>
+                <li className={styles.navbar__item}><Link to='/documentation'>Документація</Link></li>
+                <li className={styles.navbar__item}><Link to='/projects'>Проєкти</Link></li>
+                <li className={styles.navbar__item}><Link to='/gallery'>Галерея</Link></li>
+                <li className={styles.navbar__item}><Link to='/news'>Новини</Link></li>
+                <li className={styles.navbar__item}><Link to='/'>Про нас</Link></li>
+                <li className={styles.navbar__item}><Link to='/team'>Колектив</Link></li>
+            </ul>
+            <button className={styles.navbar__auth_btn}>
+                <Link to="/signup">Вхід/Реєстрація</Link>
+            </button>
 
-            </div>
         </div>
     );
 };
