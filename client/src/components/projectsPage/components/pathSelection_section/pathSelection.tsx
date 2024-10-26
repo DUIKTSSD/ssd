74933d675a6fe46d9fc6d593ab0eef00d3359e7d
Anton <fpbidni@gmail.com>
2024-10-26 12:48:05 +0100

    made projects introduce into mobile + add mobile-first approach

--- client/src/components/projectsPage/components/pathSelection_section/pathSelection.tsx
+++ client/src/components/projectsPage/components/pathSelection_section/pathSelection.tsx
@@ -8,37 +8,46 @@ const PathSelection: React.FC = () => {
     const [buttons, setButtons] = useState<HTMLDivElement | null>(null);
 
     return (
-        <div className={styles.pathSelection}>
-            <div className={styles.pathSelection__container}>
-                <div className={styles.pathSelection__title}>
-                    <h1>
-                        Твоя ідея може стати наступним проривом!
-                        <span className={styles.pathSelection__title_highlight}>
-                            Настав час діяти!
-                        </span>
-                    </h1>
-                </div>
-                <div className={styles.pathSelection__buttons_wrapper}>
-                    <h3
-                        ref={setSubtitle}
-                        className={styles.pathSelection__subtitle}
-                    >
-                        ВИБЕРИ СВІЙ ШЛЯХ
-                    </h3>
-                    <ConnectionLines
-                        subtitle={subtitle}
-                        buttons={buttons}
-                    />
-                    <div
-                        ref={setButtons}
-                        className={styles.pathSelection__buttons}
-                    >
-                        <GoToBtn url="/projectsCreation" label="Створити проєкт"/>
-                        <GoToBtn url="/projectsJoin" label="Вступити в проєкт"/>
+        <>
+            <div className={styles.pathSelection}>
+                <div className={styles.pathSelection__container}>
+                    <div className={styles.pathSelection__title}>
+                        <h1>
+                            Твоя ідея може стати наступним проривом!
+                            <p className={styles.pathSelection__title_highlight}>
+                                Настав час діяти!
+                            </p>
+                        </h1>
+                    </div>
+                    <div className={styles.pathSelection__buttons_wrapper}>
+                        <h3
+                            ref={setSubtitle}
+                            className={styles.pathSelection__subtitle}
+                        >
+                            ВИБЕРИ СВІЙ ШЛЯХ
+                        </h3>
+                        <ConnectionLines
+                            subtitle={subtitle}
+                            buttons={buttons}
+                        />
+                        <div
+                            ref={setButtons}
+                            className={`${styles.pathSelection__buttons} ${styles.pathSelection__buttons_medium}`}
+                        >
+                            <GoToBtn url="/projectsCreation" label="Створити проєкт"/>
+                            <GoToBtn url="/projectsJoin" label="Вступити в проєкт"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div
+                className={`${styles.pathSelection__buttons} ${styles.pathSelection__buttons_low}`}
+            >
+                <GoToBtn url="/projectsCreation" label="Створити проєкт"/>
+                <h3 className={styles.pathSelection__subtitle_mobile}>ВИБЕРИ СВІЙ ШЛЯХ</h3>
+                <GoToBtn url="/projectsJoin" label="Вступити в проєкт"/>
+            </div>
+        </>
     );
 }
 
