ee3d01728dde2fc8ffcf443b736fc2ab7ea82e08
Anur4ik <your_email@whatever.com>
2024-12-06 00:29:30 +0200

    add .env

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: `${import.meta.env.VITE_APP_BASE_URL}/api`,
     headers: {
         'Content-Type': 'application/json'
     },
@@ -51,7 +51,7 @@ interface AuthResponse {
 
 const auth = {
     login: async(credentials: AuthRequest): Promise<any> => {
-        const response = await axios.post('http://localhost:8080/api/auth/login', credentials);
+        const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/api/auth/login`, credentials);
         Cookies.set('token', response.data, {secure: true, sameSite: "strict"})
         console.log('Complete login ;d')
         return response.data
@@ -59,13 +59,13 @@ const auth = {
 
 
     register: async (credentials: AuthRequest): Promise<any> => {
-        const response = await axios.post<AuthResponse>(`http://localhost:8080/api/auth/register`, credentials)
+        const response = await axios.post<AuthResponse>(`${import.meta.env.VITE_APP_BASE_URL}/api/auth/register`, credentials)
         // Cookies.set('token', response.data.token, {secure: true, sameSite: "strict"})
         console.log(response.data)
         return response.data
     },
     verifi:async (credentials: AuthRequest): Promise<any> => {
-         const response = await axios.post<AuthResponse>(`http://localhost:8080/api/auth/verify`, credentials)
+         const response = await axios.post<AuthResponse>(`${import.meta.env.VITE_APP_BASE_URL}/api/auth/verify`, credentials)
          Cookies.set('token', response.data, {secure: true, sameSite: "strict"})
         console.log(response.data)
         return response.data
