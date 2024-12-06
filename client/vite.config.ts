918c1e6abf2954d13738d0f01e6d80484af2fd47
Anur4ik <your_email@whatever.com>
2024-12-06 16:51:42 +0200

    add Docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +9,6 @@ export default defineConfig({
     alias: {
       '@': './client/src', // You can set up an alias for easier imports
     },
-  },
-  define: {
-    'process.env': process.env,
   },
   server: {
     port: 8081, // Change this to your desired development port
