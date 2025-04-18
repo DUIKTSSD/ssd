ae5a0b05729ee8bc2500183441f1f485183ac2c7
Motsiiaka Denys <denzeml313@gmail.com>
2025-04-18 15:39:35 +0300

    validation on useful link and course link

--- server/Java/src/main/java/com/ssd/SSD/DTO/UsefulLinkDTO.java
+++ server/Java/src/main/java/com/ssd/SSD/DTO/UsefulLinkDTO.java
@@ -1,12 +1,16 @@
 package com.ssd.SSD.DTO;
 
 import jakarta.persistence.Lob;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UsefulLinkDTO {
+    @Size(min = 3, max=255, message = "URL повинен бути в діапазоні між 3-255 символів")
     private String url;
+
+    @Size(min = 3, max=1000, message = "URL повинен бути в діапазоні між 3-1000 символів")
     private String description;
 }
